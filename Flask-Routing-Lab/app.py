5ff58da93dab2a2e0fb667ba8f69d3d727211443
avigaille23-meet <avigaillevite@gmail.com>
2022-07-27 13:30:53 +0300

    aa

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,14 +1,23 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
+app = Flask( __name__, template_folder='templates', templates="home.html", static_folder='static'
 )
 
-@app.route('')
-def product.html 
+@app.route('product.html')
+def product.html ():
+	return render_template("product.html")
+
+<div>
+	<a href=“{{ url_for(‘product.html’) }}”>product</a>
+</div> 
+
+
+
+<div>
+	<a href=“{{ url_for(‘home.html’) }}”> home page</a>
+</div> 
+
 
 
 
