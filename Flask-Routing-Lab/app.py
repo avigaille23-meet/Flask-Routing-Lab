8743addfebd13ac6086b948360c9bc4d32f2434e
avigaille23-meet <avigaillevite@gmail.com>
2022-07-27 17:55:31 +0300

    lab

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,18 +8,14 @@ app = Flask( __name__, template_folder='templates', templates="home.html", stati
 def product.html ():
 	return render_template("product.html")
 
-<div>
-	<a href=“{{ url_for(‘product.html’) }}”>product</a>
-</div> 
-
-
-
-<div>
-	<a href=“{{ url_for(‘home.html’) }}”> home page</a>
-</div> 
-
-
-
-
-if __name__ == "__main__":  # Makes sure this is the main process
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route('cart.html')
+def cart.html ():
+	return render_template("cart.html")
+
+if __name__ == "__main__"
+	app.run(debug=true)
+	
